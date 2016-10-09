ce5af8b11a8ee7c4a7725204795f079d3ccb0f40
manhjk <luk.zielinsky@gmail.com>
2016-10-09 15:30:57 +0200

    Added jetty dependency and main method.

--- RegisterApp/src/main/java/com/example/HelloWorldServlet.java
+++ RegisterApp/src/main/java/com/example/HelloWorldServlet.java
@@ -4,15 +4,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
-public class HelloWorldServlet extends HttpServlet 
-{
-  @Override 
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException
-  {
-      response.getOutputStream().print("Hello World");
-  }
+@WebServlet(urlPatterns = { "/*" }, loadOnStartup = 1)
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 841970273993299662L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getOutputStream().print("Hello World");
+	}
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new HelloWorldServlet()), "/*");
+		server.start();
+		server.join();
+	}
 }
